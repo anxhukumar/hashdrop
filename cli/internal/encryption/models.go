ad0339ff11578b7defe69e41151aaed5fa90373b
anxhukumar <contact.anshukumar@protonmail.com>
2025-12-30 17:29:31 +0530

    Change vault and vault meta data struct

--- cli/internal/encryption/models.go
+++ cli/internal/encryption/models.go
@@ -2,12 +2,18 @@ package encryption
 
 // Vault struct to store users Data encryption keys
 type Vault struct {
+	Version int               `json:"version"`
+	Entries map[string]string `json:"entries"`
+}
+
+// Vault meta data
+type VaultMetaData struct {
 	Version int `json:"version"`
 	Argon   struct {
 		Time    uint32 `json:"time"`
 		Memory  uint32 `json:"memory"`
 		Threads uint8  `json:"threads"`
+		KeyLen  uint32 `json:"key_len"`
 	} `json:"argon"`
-	VaultSalt string            `json:"vault_salt"`
-	Entries   map[string]string `json:"entries"`
+	VaultSalt string `json:"vault_salt"`
 }
