4b76dee0cad6be58147b921f5387314eea21ad9b
anxhukumar <contact.anshukumar@protonmail.com>
2026-01-01 15:33:26 +0530

    Update files sql schema

--- server/internal/sql/schema/003_files.sql
+++ server/internal/sql/schema/003_files.sql
@@ -1,19 +1,39 @@
 -- +goose Up
 CREATE TABLE files (
     id TEXT PRIMARY KEY,
+
     user_id TEXT NOT NULL,
     file_name TEXT NOT NULL,
     mime_type TEXT,
-    hash TEXT,
-    size_bytes INTEGER,
-    hash_alg TEXT,
-    enc_alg TEXT,
-    kdf TEXT,
-    kdf_params TEXT NULL,
-    status TEXT NOT NULL DEFAULT 'pending',
+    
+    -- Integrity / verification
+    plaintext_hash TEXT,
+    plaintext_hash_version INTEGER NOT NULL DEFAULT 1,
+    
+    -- Data size stored in s3
+    plaintext_size_bytes INTEGER,
+    encrypted_size_bytes INTEGER,
+    
+    -- Ecryption metadata
+    encryption_version INTEGER NOT NULL DEFAULT 1,
+    encryption_chunk_kb INTEGER NOT NULL DEFAULT 64,
+
+    -- vault metadata
+    key_management_mode TEXT NOT NULL
+        CHECK (key_management_mode IN ('vault','passphrase')),
+    passphrase_salt TEXT,
+
+    -- Key derivation 
+    kdf_version INTEGER NOT NULL DEFAULT 1,
+
+    status TEXT NOT NULL DEFAULT 'pending'
+        CHECK (status IN ('pending', 'uploaded', 'deleted', 'failed')),
+
     s3_key TEXT NOT NULL,
+
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
+
     FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
