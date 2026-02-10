5c0a12a5e05449f7bd116a6f0ec9b40d4ca1ee57
anxhukumar <contact.anshukumar@protonmail.com>
2026-02-11 02:48:46 +0530

    Add logger in handler revoke token

--- server/internal/handlers/handler_revoke_token.go
+++ server/internal/handlers/handler_revoke_token.go
@@ -3,20 +3,38 @@ package handlers
 import "net/http"
 
 func (s *Server) HandlerRevokeToken(w http.ResponseWriter, r *http.Request) {
+	logger := s.Logger.With("handler", "handler_revoke_token")
 
 	// Get decoded refresh token from client
 	var refreshToken RefreshToken
 	if err := DecodeJson(r, &refreshToken); err != nil {
-		RespondWithError(w, s.Logger, "Invalid JSON payload", err, http.StatusBadRequest)
+		msgToDev := "user posted invalid json data"
+		msgToClient := "invalid JSON payload"
+		RespondWithWarn(
+			w,
+			logger,
+			msgToDev,
+			msgToClient,
+			err,
+			http.StatusBadRequest,
+		)
 		return
 	}
 
 	// Set the revoked_at value in refresh_tokens in database
 	err := s.Store.Queries.RevokeRefreshToken(r.Context(), refreshToken.RefreshToken)
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error revoking refresh token", err, http.StatusInternalServerError)
+		msgToDev := "error revoking refresh token in database"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+	logger.Info("refresh token revoked successfully")
 }
