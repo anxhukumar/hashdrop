7d14a8b7f6c2eeb79856e997874653e614fde93d
anxhukumar <contact.anshukumar@protonmail.com>
2026-02-11 02:36:21 +0530

    Add logger in handler login

--- server/internal/handlers/handler_login.go
+++ server/internal/handlers/handler_login.go
@@ -9,49 +9,100 @@ import (
 )
 
 func (s *Server) HandlerLogin(w http.ResponseWriter, r *http.Request) {
+	logger := s.Logger.With("handler", "handler_login")
 
 	// Get decoded incoming user login data
 	var userLoginIncoming UserLoginIncoming
 	if err := DecodeJson(r, &userLoginIncoming); err != nil {
-		RespondWithError(w, s.Logger, "Invalid JSON payload", err, http.StatusBadRequest)
+		msgToDev := "user posted invalid json data"
+		msgToClient := "invalid JSON payload"
+		RespondWithWarn(
+			w,
+			logger,
+			msgToDev,
+			msgToClient,
+			err,
+			http.StatusBadRequest,
+		)
 		return
 	}
 
 	// Check if user is registered and verified to get account details
 	userData, err := s.Store.Queries.GetVerifiedUserByEmail(r.Context(), userLoginIncoming.Email)
 	if err != nil {
-		RespondWithError(w, s.Logger, "Invalid username or password", err, http.StatusUnauthorized)
+		msgToDev := "invalid username or password during login"
+		msgToClient := "invalid username or password"
+		RespondWithWarn(
+			w,
+			logger,
+			msgToDev,
+			msgToClient,
+			err,
+			http.StatusUnauthorized,
+		)
 		return
 	}
+
+	// Attach user_id in logger context
+	logger = logger.With("user_id", userData.ID.String())
 
 	// Check if password is correct
 	isMatch, err := auth.CheckPasswordHash(userLoginIncoming.Password, userData.HashedPassword)
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error verifying password", err, http.StatusInternalServerError)
+		msgToDev := "error while verifying password hash during login"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
 	if !isMatch {
-		RespondWithError(w, s.Logger, "Invalid username or password", nil, http.StatusUnauthorized)
+		msgToDev := "password mismatch during login"
+		msgToClient := "invalid username or password"
+		RespondWithWarn(
+			w,
+			logger,
+			msgToDev,
+			msgToClient,
+			nil,
+			http.StatusUnauthorized,
+		)
 		return
 	}
 
 	// Get JWT token
 	jwtToken, err := GetJWTToken(userData, s.Cfg.JWTSecret, s.Cfg.AccessTokenExpiry)
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error creating auth token", err, http.StatusInternalServerError)
+		msgToDev := "error creating jwt access token"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
 	// Get refresh token
 	refreshToken, err := auth.MakeRefreshToken()
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error generating refresh token", err, http.StatusInternalServerError)
+		msgToDev := "error generating refresh token"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
 	// Send the refresh token to database
-
 	expiry := time.Now().Add(s.Cfg.RefreshTokenExpiry)
 
 	refreshTokenParams := database.CreateRefreshTokenParams{
@@ -61,7 +112,14 @@ func (s *Server) HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	}
 	refreshTokenData, err := s.Store.Queries.CreateRefreshToken(r.Context(), refreshTokenParams)
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error creating refresh token", err, http.StatusInternalServerError)
+		msgToDev := "error creating refresh token in database"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
@@ -72,7 +130,9 @@ func (s *Server) HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := RespondWithJSON(w, http.StatusOK, loginResponse); err != nil {
-		s.Logger.Println("failed to send login response:", err)
+		logger.Error("failed to send login response", "err", err)
 		return
 	}
+
+	logger.Info("user logged in successfully")
 }
