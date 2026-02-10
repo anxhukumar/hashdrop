91f979ac8e4ace4656f057cac4eebe4c2b08db00
anxhukumar <contact.anshukumar@protonmail.com>
2026-02-11 02:43:16 +0530

    Add logger in handler reset

--- server/internal/handlers/handler_reset.go
+++ server/internal/handlers/handler_reset.go
@@ -6,20 +6,38 @@ import (
 )
 
 func (s *Server) HandlerReset(w http.ResponseWriter, r *http.Request) {
+	logger := s.Logger.With("handler", "handler_reset")
 
 	// Check current platform to ensure data can't be reset in production
 	if s.Cfg.Platform != "dev" {
 		err := errors.New("functionality accessed from wrong dev environment")
-		RespondWithError(w, s.Logger, "Can't access this functionality without a local development environment", err, http.StatusForbidden)
+		msgToDev := "reset endpoint accessed outside dev environment"
+		msgToClient := "can't access this functionality without a local development environment"
+		RespondWithWarn(
+			w,
+			logger,
+			msgToDev,
+			msgToClient,
+			err,
+			http.StatusForbidden,
+		)
 		return
 	}
 
 	// Delete all users
 	err := s.Store.Queries.DeleteAllUsers(r.Context())
 	if err != nil {
-		RespondWithError(w, s.Logger, "Error while deleting all users", err, http.StatusInternalServerError)
+		msgToDev := "error while deleting all users from database"
+		RespondWithError(
+			w,
+			logger,
+			msgToDev,
+			err,
+			http.StatusInternalServerError,
+		)
 		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
+	logger.Info("all users deleted successfully in dev reset")
 }
